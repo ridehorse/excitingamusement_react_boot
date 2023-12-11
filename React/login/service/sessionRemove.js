723f88b45cd8a5e7fdb246ba4b6e8812c9e68774
dkstlwls <dkstlwls@gmail.com>
2023-12-11 18:15:32 +0900

    담당파트 코드입니다.

---
+++ React/login/service/sessionRemove.js
@@ -0,0 +1,10 @@
+export function SessionRemove() {
+  sessionStorage.removeItem("MEMBER_ID");
+  sessionStorage.removeItem("KAKAO_ID");
+  sessionStorage.removeItem("KAKAO_IMAGE");
+  sessionStorage.removeItem("KAKAO_NAME");
+  sessionStorage.removeItem("ADMIN");
+  sessionStorage.removeItem("GIT_ID");
+  sessionStorage.removeItem("NAVER_ID");
+  localStorage.removeItem("ACCESS_TOKEN");
+}
